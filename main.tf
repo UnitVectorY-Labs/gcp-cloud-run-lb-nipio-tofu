e9c43767772b7530d8a8394dcd7d4e26b040deac
Jared Hatfield <jjhatf02@gmail.com>
2024-10-16 18:30:17 -0400

    Cleaned up code comments

--- main.tf
+++ main.tf
@@ -6,13 +6,11 @@ locals {
   load_balancer_domain = "${var.app_name}-${replace(google_compute_global_address.load_balancer_ip.address, ".", "-")}.nip.io"
 }
 
-# Create a global static IP address for the load balancer
 resource "google_compute_global_address" "load_balancer_ip" {
   name        = "${var.app_name}-ip"
   description = "Static IP address for the load balancer"
 }
 
-# Create Artifact Registry repositories in each region
 resource "google_artifact_registry_repository" "repos" {
   for_each      = toset(var.regions)
   location      = each.key
@@ -20,6 +18,7 @@ resource "google_artifact_registry_repository" "repos" {
   description   = "Proxy Container Registry for ${var.app_name} in ${each.key}"
   format        = "DOCKER"
   mode          = "REMOTE_REPOSITORY"
+
   remote_repository_config {
     docker_repository {
       custom_repository {
@@ -29,14 +28,12 @@ resource "google_artifact_registry_repository" "repos" {
   }
 }
 
-# Service account for Cloud Run services
 resource "google_service_account" "cloud_run_sa" {
   project      = var.project_id
   account_id   = "${var.app_name}-sa"
   display_name = "${var.app_name} service account"
 }
 
-# Deploy Cloud Run services in each region
 resource "google_cloud_run_v2_service" "services" {
   for_each = toset(var.regions)
   name     = "${var.app_name}-${each.value}"
@@ -51,38 +48,38 @@ resource "google_cloud_run_v2_service" "services" {
     }
   }
 
-  depends_on = [google_artifact_registry_repository.repos]
+  depends_on = [
+    google_artifact_registry_repository.repos
+  ]
 }
 
-# Allow unauthenticated access to Cloud Run services
 resource "google_cloud_run_service_iam_member" "noauth" {
-  for_each   = toset(var.regions)
-  location   = each.key
-  service    = google_cloud_run_v2_service.services[each.key].name
-  role       = "roles/run.invoker"
-  member     = "allUsers"
+  for_each = toset(var.regions)
+  location = each.key
+  service  = google_cloud_run_v2_service.services[each.key].name
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+
   depends_on = [google_cloud_run_v2_service.services]
 }
 
-# Create Serverless NEGs for each Cloud Run service
 resource "google_compute_region_network_endpoint_group" "neg" {
   for_each              = toset(var.regions)
   name                  = "${var.app_name}-neg-${each.key}"
   region                = each.key
   network_endpoint_type = "SERVERLESS"
+
   cloud_run {
     service = google_cloud_run_v2_service.services[each.key].name
   }
 }
 
-# Define the SSL policy
 resource "google_compute_ssl_policy" "tls_policy" {
-  name = "${var.app_name}-tls-policy"
-  profile = "RESTRICTED"
+  name            = "${var.app_name}-tls-policy"
+  profile         = "RESTRICTED"
   min_tls_version = "TLS_1_2"
 }
 
-# Use the lb-http module to create a global load balancer
 module "gclb" {
   source  = "GoogleCloudPlatform/lb-http/google//modules/serverless_negs"
   version = "~> 9.0"
@@ -101,7 +98,7 @@ module "gclb" {
 
       groups = [
         {
-          group = google_compute_region_network_endpoint_group.neg[region].id # Reference the NEG
+          group = google_compute_region_network_endpoint_group.neg[region].id
         }
       ]
 
@@ -111,18 +108,14 @@ module "gclb" {
     }
   }
 
-  http_forward = false # Not supporting unencrypted traffic
+  http_forward = false
+  url_map      = "default"
+  ssl          = true
 
-  url_map = "default" # Change to a string representing the default URL map
-
-  ssl = true # Just set this to true
-
-  managed_ssl_certificate_domains = [local.load_balancer_domain] # Use the domain for the managed SSL certificate
-
-  create_address = false
-  address = google_compute_global_address.load_balancer_ip.address
-
-  ssl_policy = google_compute_ssl_policy.tls_policy.name # Reference the SSL policy
+  managed_ssl_certificate_domains = [local.load_balancer_domain]
+  create_address                  = false
+  address                         = google_compute_global_address.load_balancer_ip.address
+  ssl_policy                      = google_compute_ssl_policy.tls_policy.name
 
   depends_on = [
     google_compute_region_network_endpoint_group.neg,
